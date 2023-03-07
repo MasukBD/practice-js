e2e4d0593ae733bcb476196abb617b939e579ad7
MasukBD <imahfuz2020@outlook.com>
2023-03-07 15:57:42 +0600

    first commit

---
+++ practice1.js
@@ -0,0 +1,53 @@
+// practice 1 
+let name = 'Md Ilius Mahfuz';
+let number = 25;
+let isGraduate = true;
+
+// practice 2 
+let myAge = 25;
+const myName = 'Masuk';
+myAge = 26;
+// console.log(myAge);
+
+// practice 3 
+const num1 = 5;
+const num2 = 15;
+let sum = num1 + num2;
+let substraction = num2 - num1;
+let division = num2 / num1;
+let multiply = num1 * num2;
+let remainder = num2 % num1;
+// console.log(multiply);
+
+// practice 10
+const numbers = [23, 45, 65, 76, 34, 89, 80, 78, 90, 89, 89, 90, 100, 100, 205, 210];
+for (let i = 0; i < numbers.length; i++) {
+    let number = numbers[i];
+    if (number >= 80) {
+        // console.log(number)
+    }
+}
+
+// practice 11 
+function multiple(a, b, c) {
+    let multiplication = a * b * c;
+    return multiplication;
+}
+
+// console.log(multiple(2, 2, 2));
+
+// practice 12 
+let mobile = {
+    name: 'Samsung',
+    price: 23000,
+    storage: '64gb',
+    processor: 'Quad core'
+}
+
+// mobile.price = 20000;
+let priceChange = 'price';
+// mobile[priceChange] = 21000;
+
+// console.log(mobile.price);
+
+
